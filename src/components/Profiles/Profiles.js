319b3b9a8e873cc8ae5657fdf289c88a1118ec99
skimransabuj <sabujx9@gmail.com>
2025-07-10 23:49:43 +0600

    :up: Update: profiles card

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,92 +1,77 @@
+import React, { useState } from "react";
+import { Box, Card, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ButtonBase, Card, Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
-import { useState } from "react";
+import {
+  faGithub,
+  faLinkedin,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+
 import SectionTitle from "../../Pages/Home/AboutMe/SectionTitle";
-import { education } from "../../static/education";
-import { skillList } from "../../static/skillList";
 import { styles } from "../../theme/styles";
 
 const Profiles = () => {
-  const [profileList, setProfileList] = useState([
+  const [profileList] = useState([
     {
       name: "GitHub",
-      fontAwesomeIcon: "fa-brands fa-github",
+      icon: faGithub,
       href: "https://github.com/skImranSabuj",
     },
     {
       name: "LinkedIn",
-      fontAwesomeIcon: "fa-brands fa-linkedin",
+      icon: faLinkedin,
       href: "https://www.linkedin.com/in/skimransabuj/",
     },
     {
       name: "beecrowd",
-      fontAwesomeIcon: "fa-solid fa-laptop-code",
+      icon: faLaptopCode,
       href: "https://www.beecrowd.com.br/judge/en/profile/169674",
     },
     {
       name: "Facebook",
-      fontAwesomeIcon: "fa-brands fa-facebook",
+      icon: faFacebook,
       href: "https://www.facebook.com/skimransabuj",
     },
   ]);
 
-  const handleVisitProfile = () => {
-    console.log("clicked");
-  };
   return (
-    <Box
-      sx={
-        {
-          // border: "1px solid black"
-        }
-      }
-    >
+    <Box>
       <Box sx={{ marginBottom: "2rem" }}>
         <SectionTitle title="Profiles" />
       </Box>
-      <Box style={{ ...styles.flexCenter, flexWrap: "wrap" }}>
+
+      <Box sx={{ ...styles.flexCenter, flexWrap: "wrap", gap: "1.5rem" }}>
         {profileList.map((item, index) => (
-          // <Grid xs={3} sm={2} md={2} key={index}>
           <Card
+            key={index}
             sx={{
-              margin: "10px",
-              elevation: "0",
-              padding: "1rem",
-              width: "100px",
-              // height: "100px",
+              width: 120,
+              padding: 2,
+              textAlign: "center",
+              borderRadius: "1rem",
+              background: "linear-gradient(145deg, #f0f0f3, #cacaca)",
+              boxShadow: "5px 5px 15px #bebebe, -5px -5px 15px #ffffff",
+              transition: "all 0.3s ease-in-out",
+              "&:hover": {
+                transform: "translateY(-4px)",
+                boxShadow: "4px 4px 10px #bbb, -4px -4px 10px #fff",
+              },
             }}
-            className="skillCard"
-            onPress={() => handleVisitProfile()}
           >
             <a
               href={item.href}
               target="_blank"
               rel="noreferrer"
-              style={{ textDecoration: "none", color: "rgb(74, 74, 74)" }}
+              style={{ textDecoration: "none", color: "inherit" }}
             >
-              <Typography variant="h5">
-                <i class={item.fontAwesomeIcon}></i>
+              <Typography variant="h4" component="div" gutterBottom>
+                <FontAwesomeIcon icon={item.icon} />
               </Typography>
-              <Box
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                <Typography variant="body2">{item.name}</Typography>
-              </Box>
+              <Typography variant="body2">{item.name}</Typography>
             </a>
           </Card>
         ))}
-        {/* <Grid
-          container
-          // spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 6, sm: 8, md: 12 }}
-          // sx={{ border: "1px solid red" }}
-        >
-          
-        </Grid> */}
       </Box>
     </Box>
   );
